40e0ffea254d5b10fa0fdf21c863eb040585a7d3
IchwanArdi <ichwanpwt22@gmail.com>
2025-07-27 09:42:54 +0700

    membuat server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

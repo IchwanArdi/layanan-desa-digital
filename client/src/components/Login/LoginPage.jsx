fea77da1da525db7db90b283c13e647a9e15aca6
IchwanArdi <ichwanpwt22@gmail.com>
2025-07-28 05:42:59 +0700

    fixed konfigurasi backend

--- client/src/components/Login/LoginPage.jsx
+++ client/src/components/Login/LoginPage.jsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
 
   const handleLogin = async (data) => {
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- client/src/components/Login/RegisterPage.jsx
+++ client/src/components/Login/RegisterPage.jsx
@@ -5,7 +5,7 @@ export default function RegisterPage() {
   const navigate = useNavigate();
   const handleRegister = async (data) => {
     try {
-      const response = await fetch('http://localhost:3000/api/register', {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

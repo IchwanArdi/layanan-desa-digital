65dc76b11e0a037b9e730b85777d99dee89ab24f
IchwanArdi <ichwanpwt22@gmail.com>
2025-07-28 04:57:28 +0700

    fixed backend

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const authRoutes = require('./routes/auth'); // ganti sesuai nama file router kamu
+const session = require('express-session');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 // Connect ke MongoDB
 connectDB();
@@ -14,6 +17,18 @@ app.use(
   cors({
     origin: 'http://localhost:5173', // frontend
     credentials: true,
+  })
+);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'fallback-secret',
+    resave: false,
+    saveUninitialized: true,
+    // Tidak ada store, akan menggunakan memory store default
+    cookie: {
+      maxAge: 14 * 24 * 60 * 60 * 1000, // 14 hari dalam milliseconds
+    },
   })
 );
 

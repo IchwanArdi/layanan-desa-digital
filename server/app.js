01cbcf28c0ffc0146f8a6fc7f52f54a8c8a45807
IchwanArdi <ichwanpwt22@gmail.com>
2025-08-05 12:10:57 +0700

    mengganti domain

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://layanan-desa-digital.vercel.app'], // frontend
+    origin: ['http://localhost:5173', 'https://layanandesa.vercel.app'], // frontend
     credentials: true,
   })
 );
